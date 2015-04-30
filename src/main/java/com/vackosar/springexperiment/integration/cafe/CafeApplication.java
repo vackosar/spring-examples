ac39837abb2676f78f5b52e19f8fcaf22b246941
vackosar <vackosar>
2015-04-30 22:44:37 +0200

    Refactors DSL Cafe Application.

--- src/main/java/com/vackosar/springexperiment/integration/cafe/CafeApplication.java
+++ src/main/java/com/vackosar/springexperiment/integration/cafe/CafeApplication.java
@@ -1,7 +1,7 @@
 package com.vackosar.springexperiment.integration.cafe;
 
+import java.util.Collection;
 import java.util.concurrent.Executors;
-
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -9,13 +9,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.aggregator.MessageGroupProcessor;
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.dsl.AggregatorSpec;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.dsl.support.Consumer;
+import org.springframework.integration.router.AbstractMessageRouter;
 import org.springframework.integration.scheduling.PollerMetadata;
 import org.springframework.integration.stream.CharacterStreamWritingMessageHandler;
+import org.springframework.integration.transformer.GenericTransformer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+
 import java.util.stream.Collectors;
 
 /**
@@ -71,24 +79,37 @@ public class CafeApplication {
   	return f -> f                                               // 13
   	  .split(Order.class, Order::getItems)                      // 14
   	  .channel(c -> c.executor(Executors.newCachedThreadPool()))// 15
-  	  .<OrderItem, Boolean>route(OrderItem::isIced, mapping -> mapping // 16
+  	  .<OrderItem, Boolean>route(
+  			  OrderItem::isIced, mapping -> mapping // 16
   	    .subFlowMapping("true", routeIced())// 24
   	    .subFlowMapping("false", routeNotIced()))
-  	  .<OrderItem, Drink>transform(orderItem ->
+  	  .transform(getOrderItemToDrinkTransformer())                                // 27
+  	  .aggregate(getDrinksToDelivery(), null)     // 31
+  	  .handle(CharacterStreamWritingMessageHandler.stdout());   // 32
+  }
+
+private Consumer<AggregatorSpec> getDrinksToDelivery() {
+	return aggregator -> aggregator                       // 28
+  	    .outputProcessor(groupDrinksToDelivery())                     // 30
+  	    .correlationStrategy(m ->
+  	      ((Drink) m.getPayload()).getOrderNumber());
+}
+
+private MessageGroupProcessor groupDrinksToDelivery() {
+	return group ->                               // 29
+	  new Delivery(group.getMessages()
+	    .stream()
+	    .map(message -> (Drink) message.getPayload())
+	    .collect(Collectors.toList()));
+}
+
+private GenericTransformer<OrderItem, Drink> getOrderItemToDrinkTransformer() {
+	return orderItem ->
   	    new Drink(orderItem.getOrderNumber(),
   	      orderItem.getDrinkType(),
   	      orderItem.isIced(),
-  	      orderItem.getShots()))                                // 27
-  	  .aggregate(aggregator -> aggregator                       // 28
-  	    .outputProcessor(group ->                               // 29
-  	      new Delivery(group.getMessages()
-  	        .stream()
-  	        .map(message -> (Drink) message.getPayload())
-  	        .collect(Collectors.toList())))                     // 30
-  	    .correlationStrategy(m ->
-  	      ((Drink) m.getPayload()).getOrderNumber()), null)     // 31
-  	  .handle(CharacterStreamWritingMessageHandler.stdout());   // 32
-  }
+  	      orderItem.getShots());
+}
 
 private IntegrationFlow routeNotIced() {
 	return sf -> sf                        // 25
