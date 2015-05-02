b6df9778a6ed5451dc6537d73f04e8f27e9b9a43
vackosar <vackosar>
2015-05-02 09:01:13 +0200

    Refactors indentation.

--- src/main/java/com/vackosar/springexperiment/integration/cafe/CafeApplication.java
+++ src/main/java/com/vackosar/springexperiment/integration/cafe/CafeApplication.java
@@ -46,10 +46,10 @@ public static void main(String[] args) throws Exception {
 
 	Cafe cafe = ctx.getBean(Cafe.class);                         
 	for (int i = 1; i <= 100; i++) {                             
- Order order = new Order(i);
- order.addItem(DrinkType.LATTE, 2, false); 
- order.addItem(DrinkType.MOCHA, 3, true);  
- cafe.placeOrder(order);
+		Order order = new Order(i);
+		order.addItem(DrinkType.LATTE, 2, false); 
+		order.addItem(DrinkType.MOCHA, 3, true);  
+		cafe.placeOrder(order);
 	}
 
 	System.out.println("Hit 'Enter' to terminate");              
